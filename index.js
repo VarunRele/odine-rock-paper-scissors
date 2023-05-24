699517baa93889a4c286ac5749c61163a8b301ea
VarunRele <rele.varun1@gmail.com>
2023-05-24 18:41:23 +0530

    Added UI using JS

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+let i = 0
+let win = 0
+const btns = document.querySelectorAll("button")
+const comp = document.querySelector(".comp")
+const player = document.querySelector(".player")
+const result = document.querySelector(".result")
+const score = document.querySelector(".score")
+
 getComputerChoice = () => {
     let choices = ['rock', 'paper', 'scissors']
     return choices[Math.floor(Math.random() * choices.length)]
@@ -5,41 +13,50 @@ getComputerChoice = () => {
 
 playRound = (playerSelection, computerSelection) => {
     if (playerSelection === 'rock' && computerSelection === 'paper') {
-        return 'You Lose! Paper beats Rock'
+        return ['You Lose! Paper beats Rock', 0]
     } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
-        return 'You Win! Rock beats Scissors'
+        return ['You Win! Rock beats Scissors', 1]
     } else if (playerSelection === 'paper' && computerSelection === 'rock') {
-        return 'You Win! Paper beats Rock'
+        return ['You Win! Paper beats Rock', 1]
     } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
-        return 'You Lose! Scissors beats Paper'
+        return ['You Lose! Scissors beats Paper', 0]
     } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
-        return 'You Win! Scissors beats Paper'
+        return ['You Win! Scissors beats Paper', 1]
     } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
-        return 'You Lose! Rock beats Scissors'
+        return ['You Lose! Rock beats Scissors', 0]
     } else {
-        return "It's a tie"
+        return ["It's a tie", 0]
     }
 }
 
 getPlayerChoice = () => {
-    let value = prompt("Select \n1.Rock\n2.Paper\n3.Scissors")
-    if (value === '1') {
-        return 'rock'
-    } else if (value === '2') {
-        return 'paper'
-    } else if (value === '3') {
-        return 'scissors'
-    }
+    // let value = prompt("Select \n1.Rock\n2.Paper\n3.Scissors")
+    // if (value === '1') {
+    //     return 'rock'
+    // } else if (value === '2') {
+    //     return 'paper'
+    // } else if (value === '3') {
+    //     return 'scissors'
+    // }
 }
 
-game = () => {
-    for(let i = 0; i < 5; i++) {
-        let computerSelection = getComputerChoice()
-        let playerSelection = getPlayerChoice()
-        console.log("Computer "+ computerSelection)
-        console.log("Player " + playerSelection)
-        console.log(playRound(playerSelection, computerSelection))
-    }
+game = (playerSelection) => {
+    let computerSelection = getComputerChoice()
+    // let playerSelection = getPlayerChoice()
+    comp.innerText = "Computer: "+ computerSelection
+    player.innerText = "Player " + playerSelection
+    let p = playRound(playerSelection, computerSelection)
+    result.innerText = p[0]
+    i += 1
+    win += p[1]
+    score.innerText = `You have won: ${win}/${i} games`
 }
 
-game()
+
+btns.forEach(btn => {
+    btn.addEventListener('click', (e) => {
+        game(e.target.innerText.toLowerCase())
+    })
+})
+
+// game()
